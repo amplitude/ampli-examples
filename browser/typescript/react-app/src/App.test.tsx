46b51ab2ffef0c3a12988377714e7a2dac46a72f
justin.fiedler <justin.fiedler@amplitude.com>
2021-11-30 17:06:57 +0000

    set dummy amplitude api key for react tests

--- browser/typescript/react-app/src/App.test.tsx
+++ browser/typescript/react-app/src/App.test.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+beforeAll(() => {
+  process.env.REACT_APP_AMPLITUDE_API_KEY = 'test-amplitude-api-key'
+})
+
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/Ampli Browser TypeScript Example with React/i);
