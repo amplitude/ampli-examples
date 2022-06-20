1128eabdcc2ba7fa1160274e199b6a8b22a455fb
Dean Shi <dean.xiaoshi@gmail.com>
2022-06-20 02:13:01 -0700

    feat: add ampli v2 example

---
+++ browser/typescript/v2/react-app/src/App.tsx
@@ -0,0 +1,198 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import { DefaultOptions, EventWithOptionalProperties, ampli } from './ampli';
+
+const { REACT_APP_AMPLITUDE_API_KEY = '', REACT_APP_SEGMENT_WRITE_KEY = '' } = process.env;
+
+const userId = 'ampli-browser-ts-user-id';
+
+/**
+ * Start by calling ampli.load()
+ *
+ * 'ampli' is the default instance of Ampli()
+ */
+
+/**
+ * When you pull your tracking plan you can use the defaults and call load() without arguments
+ *
+ * This requires connecting your account via `ampli pull`
+ * which will set you API key in the generated Ampli SDK
+ */
+// ampli.load();
+
+/**
+ * OR Specify an environment
+ */
+// ampli.load({ environment: 'development' })
+
+/** OR Provide a specific Amplitude API key */
+// ampli.load({ client: { apiKey: REACT_APP_AMPLITUDE_API_KEY } })
+
+/**
+ * OR Use an existing Amplitude instance
+ * requires "import amplitude from 'amplitude-js';"
+ */
+// const instance = amplitude.getInstance();
+// instance.init(REACT_APP_AMPLITUDE_API_KEY, undefined, { ...DefaultOptions, logLevel: "INFO" });
+// ampli.load({ client: { instance } });
+
+/**
+ * OR Use an existing window.amplitude instance from code snippet (not recommended)
+ */
+// const instance = window.amplitude.getInstance().init(REACT_APP_AMPLITUDE_API_KEY);
+// ampli.load({ client: { instance: instance as any } });
+
+/**
+ * OR Specify AmplitudeClient 'options'
+ */
+ampli.load({
+  client: {
+    apiKey: REACT_APP_AMPLITUDE_API_KEY,
+    options: { ...DefaultOptions, logLevel: 3 },
+  },
+});
+
+/**
+ * For testing you can disable ampli
+ */
+// ampli.load({
+//   disabled: process.env.IS_TESTING ? true : false,
+// });
+
+/**
+ * Make as many Ampli instances as you want
+ */
+// const ampli2 = new Ampli();
+// ampli2.load({ client: { apiKey: 'api-key-2' } });
+
+/**
+ * 3rd party destination support
+ */
+// const segmentMiddleware = getSegmentMiddleware(REACT_APP_SEGMENT_WRITE_KEY);
+// ampli.addEventMiddleware(segmentMiddleware);
+
+/**
+ * Legacy Itly Plugins can also be adapted to middleware
+ * WARNING: This is not recommended, but is shown for legacy Itly customers as an example.
+ * WARNING: Instead, we recommend directly integrating with 3rd party destinations
+ * WARNING: in Middleware like in the getSegmentMiddleware above.
+ */
+// const segmentItlyPluginMiddleware = getSegmentItlyPluginMiddleware(REACT_APP_SEGMENT_WRITE_KEY);
+// ampli.addEventMiddleware(segmentItlyPluginMiddleware);
+
+/**
+ * Middleware can also modify the event stream
+ * Adding stop middleware will prevent events from going to Amplitude
+ */
+// ampli.addEventMiddleware(stopMiddleware);
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Ampli Browser TypeScript Example with React</h2>
+
+        <button onClick={() => ampli.identify(userId, { requiredNumber: 42 })}>Identify</button>
+
+        <button onClick={() => ampli.setGroup('test group', 'browser-ts-ampli')}>Group</button>
+
+        <button onClick={() => ampli.groupIdentify('test group', 'browser-ts-ampli', { requiredBoolean: true })}>
+          Group Identify
+        </button>
+
+        <button onClick={() => ampli.track(new EventWithOptionalProperties({ optionalBoolean: true }))}>
+          Event w/ Optional Properties
+        </button>
+
+        <button
+          onClick={() => {
+            ampli.eventWithAllProperties({
+              requiredNumber: 1.23,
+              requiredArray: ["I'm", 'required'],
+              requiredBoolean: false,
+              requiredEnum: 'Enum1',
+              requiredInteger: 42,
+              requiredString: 'Hi!',
+            });
+          }}
+        >
+          Event w/ All Properties
+        </button>
+
+        <button
+          onClick={() => {
+            ampli.track(
+              new EventWithOptionalProperties({
+                optionalString: 'Event with segment middleware extras',
+              }),
+              undefined,
+            );
+          }}
+        >
+          Event w/ Segment
+        </button>
+
+        <button
+          onClick={() => {
+            ampli.eventNoProperties();
+
+            ampli.eventWithConstTypes();
+
+            ampli.eventWithOptionalProperties({
+              optionalBoolean: true,
+            });
+
+            ampli.eventMaxIntForTest({
+              intMax10: 5,
+            });
+
+            ampli.eventObjectTypes({
+              requiredObject: { 'key-1': 'value-1' },
+              requiredObjectArray: [{ 'key-1': 'value-1' }, { 'key-2': 'value-2' }],
+            });
+
+            ampli.eventWithArrayTypes({
+              requiredBooleanArray: [true, false],
+              requiredNumberArray: [1.2, 3, 4.56],
+              requiredObjectArray: [{ 'key-1': 'value-1' }, { 'key-2': 'value-2' }],
+              requiredStringArray: ['string-1', 'string-2', 'string-3'],
+            });
+
+            ampli.eventWithEnumTypes({
+              'required enum': 'required enum 2',
+            });
+
+            ampli.eventWithOptionalArrayTypes({
+              optionalBooleanArray: [true, false],
+            });
+
+            ampli.eventWithTemplateProperties({
+              required_event_property: 'event property',
+              required_template_property: 'template property',
+              optional_template_property: 1.23,
+            });
+
+            ampli.eventWithDifferentCasingTypes({
+              'enum with space': 'enum with space',
+              enum_snake_case: 'enum_snake_case',
+              enumCamelCase: 'enumCamelCase',
+              EnumPascalCase: 'EnumPascalCase',
+              'property with space': 'property with space',
+              property_with_snake_case: 'property with snake case',
+              propertyWithCamelCase: 'property with camel case',
+              PropertyWithPascalCase: 'property with pascal case',
+            });
+          }}
+        >
+          Other Events
+        </button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ browser/typescript/v2/react-app/src/ampli.test.ts
@@ -0,0 +1,140 @@
+import { Ampli, ApiKey } from './ampli';
+import * as amplitude from '@amplitude/analytics-browser';
+
+describe('Ampli Browser TS SDK V2 tests', () => {
+  let ampli: Ampli;
+  let userId = 'test-browser-ts-ampli-v2-user-id';
+  let consoleLogMock: jest.SpyInstance;
+  let consoleErrorMock: jest.SpyInstance;
+
+  beforeAll(() => {
+    global.fetch = jest
+      .spyOn(global, 'fetch')
+      .mockImplementation(() => Promise.resolve<any>({ json: () => Promise.resolve() }));
+  });
+
+  beforeEach(() => {
+    consoleLogMock = jest.spyOn(console, 'log').mockImplementation();
+    consoleErrorMock = jest.spyOn(console, 'error').mockImplementation();
+
+    ampli = new Ampli();
+    // Set API keys for tests
+    ApiKey.production = 'test-api-key-prod';
+    ApiKey.development = 'test-api-key-dev';
+  });
+
+  afterAll(() => {
+    (global.fetch as jest.Mock<any, any>).mockClear();
+  });
+
+  afterEach(() => {
+    consoleLogMock.mockRestore();
+    consoleErrorMock.mockRestore();
+  });
+
+  test('should load() without any arguments if there are ApiKeys for each environment', () => {
+    expect(() => ampli.load()).not.toThrow();
+  });
+
+  test('should log warning if load() without any arguments without ApiKeys for each environment', () => {
+    ApiKey.production = '';
+    ApiKey.development = '';
+    ampli.load();
+
+    expect(consoleLogMock).toHaveBeenCalledTimes(0);
+    expect(consoleErrorMock).toHaveBeenCalledTimes(1);
+    expect(consoleErrorMock.mock.calls).toEqual([
+      [`ERROR: ampli.load() requires 'environment', 'client.apiKey', or 'client.instance'`],
+    ]);
+  });
+
+  test('should identify()', async () => {
+    await ampli.load()?.promise;
+
+    const response = (await ampli.identify(userId, {
+      optionalArray: ['A', 'ray'],
+      requiredNumber: 42,
+    })?.promise) as amplitude.Types.Result;
+
+    expect(response.event.event_type).toBe('$identify');
+    expect(response.event.user_properties).toEqual({
+      $set: {
+        optionalArray: ['A', 'ray'],
+        requiredNumber: 42,
+      },
+    });
+  });
+
+  test('should setGroup()', async () => {
+    const mockAmp = { setGroup: jest.fn() };
+
+    await ampli.load({ client: { instance: mockAmp as unknown as typeof amplitude } })?.promise;
+
+    ampli.setGroup('Group name', 'Group Value');
+
+    const setGroupCalls = mockAmp.setGroup.mock.calls;
+    expect(setGroupCalls.length).toBe(1);
+    expect(setGroupCalls[0]).toEqual(['Group name', 'Group Value']);
+  });
+
+  test('should groupIdentify()', async () => {
+    const mockAmp = { groupIdentify: jest.fn() };
+
+    await ampli.load({ client: { instance: mockAmp as unknown as typeof amplitude } })?.promise;
+
+    await ampli.groupIdentify('Group name', 'Group Value', {
+      requiredBoolean: true,
+      optionalString: 'some-string',
+    })?.promise;
+
+    const groupIdentifyCalls = mockAmp.groupIdentify.mock.calls;
+    expect(groupIdentifyCalls.length).toBe(1);
+    expect(groupIdentifyCalls[0]).toEqual([
+      'Group name',
+      'Group Value',
+      {
+        _propertySet: new Set(['requiredBoolean', 'optionalString']),
+        _properties: {
+          $set: {
+            optionalString: 'some-string',
+            requiredBoolean: true,
+          },
+        },
+      },
+      undefined,
+    ]);
+  });
+
+  test('should track an event with no properties', async () => {
+    await ampli.load()?.promise;
+
+    const response = await ampli.eventNoProperties()?.promise;
+
+    expect((response as amplitude.Types.Result).event.event_type).toBe('Event No Properties');
+    expect((response as amplitude.Types.Result).event.event_properties).toBe(undefined);
+  });
+
+  test('should track an event with properties of all types', async () => {
+    await ampli.load()?.promise;
+
+    const response = await ampli.eventWithAllProperties({
+      requiredBoolean: false,
+      requiredInteger: 42,
+      requiredEnum: 'Enum1',
+      requiredNumber: 42.0,
+      requiredString: 'Required string',
+      requiredArray: ['Required', 'string'],
+    })?.promise;
+
+    expect((response as amplitude.Types.Result).event.event_type).toBe('Event With All Properties');
+    expect((response as amplitude.Types.Result).event.event_properties).toEqual({
+      requiredBoolean: false,
+      requiredConst: 'some-const-value',
+      requiredInteger: 42,
+      requiredEnum: 'Enum1',
+      requiredNumber: 42.0,
+      requiredString: 'Required string',
+      requiredArray: ['Required', 'string'],
+    });
+  });
+});
