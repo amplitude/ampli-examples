ac0454f429b5b3ce3794ce54d7b2f87c6652cc23
Dean Shi <dean.xiaoshi@gmail.com>
2022-06-22 23:06:55 -0700

    AMP-53883 add ampli js v2 example

---
+++ browser/javascript/v2/react-app/README.md
@@ -0,0 +1,38 @@
+# Ampli Browser SDK (2.0)
+
+An example React app using the Ampli Browser SDK V2. Based on create-react-app.
+
+# Usage
+
+### Setup the project
+
+You will need to do the following before running the app.
+
+1. Create a `.env` with your API keys
+   1. `cp .env.example .env`
+   2. Set your Amplitude API key
+   3. (Optional) To use Segment plugin set a Segment write key
+2. `yarn install`
+
+### Run the app
+
+You can run the app using the events from our sample tracking plan. This will log events to your Amplitude project.
+
+- `yarn start`
+
+### Pull your Ampli SDK
+
+If you want to use your own tracking plan and Ampli SDK
+
+1. `npm i -g @amplitude/ampli-v2`
+2. `ampli pull`
+3. Update `app.js` to use the events from your tracking plan
+
+# Project structure
+
+- README.md - you are here \*
+- - src/
+- [App.js](src/App.js) - Example user app using Amply SDK. A good place to start.
+- ampli/
+  - [index.js](src/ampli/index.js) - Generated SDK, don't modify by hand. Update with `ampli pull`
+  - [schema.json](src/ampli/schema.json) - The full event schema for the tracking plan
---
+++ browser/javascript/v2/react-app/src/ampli.test.js
@@ -0,0 +1,215 @@
+import { Ampli, ApiKey } from './ampli';
+
+describe('Ampli Browser JS SDK V2 tests', () => {
+  /** @typedef {Ampli}*/
+  let ampli;
+  let userId = 'test-browser-js-ampli-user-id';
+  let consoleLogMock;
+  let consoleErrorMock;
+
+  beforeEach(() => {
+    consoleLogMock = jest.spyOn(console, 'log').mockImplementation();
+    consoleErrorMock = jest.spyOn(console, 'error').mockImplementation();
+
+    ampli = new Ampli();
+    // Set API keys for tests
+    ApiKey.production = 'test-api-key-prod';
+    ApiKey.development = 'test-api-key-dev';
+  });
+
+  afterEach(() => {
+    consoleLogMock.mockRestore();
+    consoleErrorMock.mockRestore();
+  });
+
+  test('should load() without any arguments if there are ApiKeys for each environment', () => {
+    expect(() => ampli.load()).not.toThrow();
+
+    expect(consoleLogMock).toHaveBeenCalledTimes(0);
+    expect(consoleErrorMock).toHaveBeenCalledTimes(0);
+  });
+
+  test('should log warning if load() without any arguments without ApiKeys for each environment', () => {
+    ApiKey.production = '';
+    ApiKey.development = '';
+    ampli.load();
+
+    expect(consoleLogMock).toHaveBeenCalledTimes(0);
+    expect(consoleErrorMock).toHaveBeenCalledTimes(1);
+    expect(consoleErrorMock.mock.calls).toEqual([
+      [`ERROR: ampli.load() requires 'environment', 'client.apiKey', or 'client.instance'`],
+    ]);
+  });
+
+  test('should identify()', async () => {
+    const send = jest.fn().mockReturnValueOnce({
+      status: 'success',
+      statusCode: 200,
+      body: {
+        eventsIngested: 1,
+        payloadSizeBytes: 1,
+        serverUploadTime: 1,
+      },
+    });
+    await ampli.load({
+      client: {
+        options: {
+          transportProvider: {
+            send,
+          },
+        },
+      },
+    })?.promise;
+    const response = await ampli.identify(userId, {
+      optionalArray: ['A', 'ray'],
+      requiredNumber: 42,
+    })?.promise;
+
+    expect(response.event.event_type).toBe('$identify');
+    expect(response.event.user_id).toBe(userId);
+    expect(response.event.user_properties).toEqual({
+      $set: {
+        optionalArray: ['A', 'ray'],
+        requiredNumber: 42,
+      },
+    });
+    expect(consoleLogMock).toHaveBeenCalledTimes(0);
+    expect(consoleErrorMock).toHaveBeenCalledTimes(0);
+  });
+
+  test('should setGroup()', () => {
+    const mockAmp = { setGroup: jest.fn() };
+
+    ampli.load({ client: { instance: mockAmp } });
+
+    ampli.setGroup('Group name', 'Group Value');
+
+    const setGroupCalls = mockAmp.setGroup.mock.calls;
+    expect(setGroupCalls.length).toBe(1);
+    expect(setGroupCalls[0]).toEqual(['Group name', 'Group Value']);
+
+    expect(consoleLogMock).toHaveBeenCalledTimes(0);
+    expect(consoleErrorMock).toHaveBeenCalledTimes(0);
+  });
+
+  test('should groupIdentify()', async () => {
+    const mockAmp = { groupIdentify: jest.fn() };
+    const send = jest.fn().mockReturnValueOnce({
+      status: 'success',
+      statusCode: 200,
+      body: {
+        eventsIngested: 1,
+        payloadSizeBytes: 1,
+        serverUploadTime: 1,
+      },
+    });
+    await ampli.load({
+      client: {
+        instance: mockAmp,
+        options: {
+          transportProvider: {
+            send,
+          },
+        },
+      },
+    })?.promise;
+
+    await ampli.groupIdentify('Group name', 'Group Value', {
+      requiredBoolean: true,
+      optionalString: 'some-string',
+    })?.promise;
+
+    const groupIdentifyCalls = mockAmp.groupIdentify.mock.calls;
+    expect(groupIdentifyCalls.length).toBe(1);
+    expect(groupIdentifyCalls[0]).toEqual([
+      'Group name',
+      'Group Value',
+      {
+        _propertySet: new Set(['requiredBoolean', 'optionalString']),
+        _properties: {
+          $set: {
+            optionalString: 'some-string',
+            requiredBoolean: true,
+          },
+        },
+      },
+      undefined,
+    ]);
+
+    expect(consoleLogMock).toHaveBeenCalledTimes(0);
+    expect(consoleErrorMock).toHaveBeenCalledTimes(0);
+  });
+
+  test('should track an event with no properties', async () => {
+    const send = jest.fn().mockReturnValueOnce({
+      status: 'success',
+      statusCode: 200,
+      body: {
+        eventsIngested: 1,
+        payloadSizeBytes: 1,
+        serverUploadTime: 1,
+      },
+    });
+    await ampli.load({
+      client: {
+        options: {
+          transportProvider: {
+            send,
+          },
+        },
+      },
+    })?.promise;
+
+    const response = await ampli.eventNoProperties()?.promise;
+
+    expect(response.event.event_type).toBe('Event No Properties');
+    expect(response.event.event_properties).toBe(undefined);
+
+    expect(consoleLogMock).toHaveBeenCalledTimes(0);
+    expect(consoleErrorMock).toHaveBeenCalledTimes(0);
+  });
+
+  test('should track an event with properties of all types', async () => {
+    const send = jest.fn().mockReturnValueOnce({
+      status: 'success',
+      statusCode: 200,
+      body: {
+        eventsIngested: 1,
+        payloadSizeBytes: 1,
+        serverUploadTime: 1,
+      },
+    });
+    await ampli.load({
+      client: {
+        options: {
+          transportProvider: {
+            send,
+          },
+        },
+      },
+    })?.promise;
+
+    const response = await ampli.eventWithAllProperties({
+      requiredBoolean: false,
+      requiredInteger: 42,
+      requiredEnum: 'Enum1',
+      requiredNumber: 42.0,
+      requiredString: 'Required string',
+      requiredArray: ['Required', 'string'],
+    })?.promise;
+
+    expect(response.event.event_type).toBe('Event With All Properties');
+    expect(response.event.event_properties).toEqual({
+      requiredBoolean: false,
+      requiredConst: 'some-const-value',
+      requiredInteger: 42,
+      requiredEnum: 'Enum1',
+      requiredNumber: 42.0,
+      requiredString: 'Required string',
+      requiredArray: ['Required', 'string'],
+    });
+
+    expect(consoleLogMock).toHaveBeenCalledTimes(0);
+    expect(consoleErrorMock).toHaveBeenCalledTimes(0);
+  });
+});
