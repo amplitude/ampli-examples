5285a6978179760f5c6d7e7419da48974820f9bb
Xinyi Ye <xinyi.ye@amplitude.com>
2022-08-17 10:53:37 -0700

    Update ampli-example.go

--- go/simple/v1/example/ampli-example.go
+++ go/simple/v1/example/ampli-example.go
@@ -13,41 +13,19 @@ func main() {
 	if err != nil {
 		log.Fatalln("Error loading .env file")
 	}
-	println(os.Getenv("AMPLITUDE_API_KEY"))
 	apiKey := os.Getenv("AMPLITUDE_API_KEY")
-	//writeKey := os.Getenv("SEGMENT_WRITE_KEY")
 
 	// Initialize the Ampli instance with LoadOptions and LoadClientOptions
 	ampli.Client.Load(ampli.LoadOptions{
 		Environment: ampli.DEVELOPMENT,
 		Disabled:    false,
 		Client: ampli.LoadClientOptions{
-			APIKey:   apiKey,
-			Instance: nil,
-			Configuration: amplitude.Config{
-				ServerZone: amplitude.ServerZoneEU,
-			},
+			APIKey:        "",
+			Instance:      nil,
+			Configuration: amplitude.NewConfig(apiKey),
 		},
 	})
 
-	// Identify using IdentifyProperties in tracking plan
-	//ampli.Client.Identify("user_id", ampli.Identify{
-	//	RequiredNumber: 16.6,
-	//	OptionalArray:  []string{"abc", "test"},
-	//}, amplitude.EventOptions{})
-	//
-	//// Group Identify using GroupProperties in tracking plan
-	//ampli.Client.GroupIdentify("Org", "Engineer", ampli.Group{
-	//	RequiredBoolean: true,
-	//	OptionalString:  "",
-	//}, amplitude.EventOptions{})
-
-	// Set groups for user
-	//ampli.Client.SetGroup("user_id", "Org", []string{"Engineer", "DevOp"}, amplitude.EventOptions{})
-
-	// Track strongly typed event class with ampli.track
-	ampli.Client.Track("", &ampli.EventMaxIntForTest{}, amplitude.EventOptions{UserID: "user_id", DeviceID: "device_id"})
-
 	// Track event with strongly typed method
-	ampli.Client.EventNoProperties("user_id", amplitude.EventOptions{DeviceID: "device_id"})
+	ampli.Client.EventNoProperties("user_id", amplitude.EventOptions{})
 }
