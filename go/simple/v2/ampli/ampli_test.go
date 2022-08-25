e5736bd8361ad58ca669985a61d8cecd17ae8672
Xinyi Ye <xinyi.ye@amplitude.com>
2022-08-25 14:43:55 -0700

    add tests for ampli.load()

--- go/simple/v2/ampli/ampli_test.go
+++ go/simple/v2/ampli/ampli_test.go
@@ -1,7 +1,6 @@
 package ampli
 
 import (
-	"log"
 	"testing"
 
 	"github.com/amplitude/analytics-go/amplitude"
@@ -77,10 +76,73 @@ func TestEventWithAllProperties(t *testing.T) {
 	eventProperties["optionalString"] = "optional-string"
 	// Should not have optionalBool in EventProperties
 	assert.Equal(t, eventProperties, event.toAmplitudeEvent().EventProperties)
-	log.Printf("TestEventWithAllProperties: \n\t Should NOT have optionalBool in EventProperties: \n\t %+v", event.toAmplitudeEvent().EventProperties)
+	//log.Printf("TestEventWithAllProperties: \n\t Should NOT have optionalBool in EventProperties: \n\t %+v", event.toAmplitudeEvent().EventProperties)
 
 	event.SetOptionalBool(false)
 	eventProperties["optionalBool"] = false
 	assert.Equal(t, eventProperties, event.toAmplitudeEvent().EventProperties)
-	log.Printf("TestEventWithAllProperties: \n\t Should have optionalBool in EventProperties: \n\t %+v", event.toAmplitudeEvent().EventProperties)
+	//log.Printf("TestEventWithAllProperties: \n\t Should have optionalBool in EventProperties: \n\t %+v", event.toAmplitudeEvent().EventProperties)
+}
+
+func TestAmpli_LoadWithEnvironment(t *testing.T) {
+	expectConfig := amplitude.NewConfig("test-development-api-key")
+
+	instance := Ampli{}
+	APIKey[EnvironmentDevelopment] = "test-development-api-key"
+	instance.Load(LoadOptions{
+		Environment: EnvironmentDevelopment,
+	})
+
+	// Amplitude.Client.Logger is an interface of which equality is determined based on memory address
+	// So here only check one of the default configurations, FlushQueueSize
+	assert.Equal(t, expectConfig.APIKey, instance.Client.GetConfig().APIKey)
+	assert.Equal(t, expectConfig.FlushQueueSize, instance.Client.GetConfig().FlushQueueSize)
+}
+
+func TestAmpli_LoadWithClient(t *testing.T) {
+	expectConfig := amplitude.NewConfig("test-development-api-key")
+
+	instance := Ampli{}
+	instance.Load(LoadOptions{
+		Client: LoadClientOptions{
+			APIKey: "test-development-api-key",
+		},
+	})
+
+	// Amplitude.Client.Logger is an interface of which equality is determined based on memory address
+	// So here only check one of the default configurations, FlushQueueSize
+	assert.Equal(t, expectConfig.APIKey, instance.Client.GetConfig().APIKey)
+	assert.Equal(t, expectConfig.FlushQueueSize, instance.Client.GetConfig().FlushQueueSize)
+}
+
+func TestAmpli_LoadWithInstance(t *testing.T) {
+	expectConfig := amplitude.NewConfig("test-development-api-key")
+
+	instance := Ampli{}
+	instance.Load(LoadOptions{
+		Client: LoadClientOptions{
+			Instance: amplitude.NewClient(amplitude.NewConfig("test-development-api-key")),
+		},
+	})
+
+	// Amplitude.Client.Logger is an interface of which equality is determined based on memory address
+	// So here only check one of the default configurations, FlushQueueSize
+	assert.Equal(t, expectConfig.APIKey, instance.Client.GetConfig().APIKey)
+	assert.Equal(t, expectConfig.FlushQueueSize, instance.Client.GetConfig().FlushQueueSize)
+}
+
+func TestAmpli_LoadWithConfig(t *testing.T) {
+	expectConfig := amplitude.NewConfig("test-development-api-key")
+
+	instance := Ampli{}
+	instance.Load(LoadOptions{
+		Client: LoadClientOptions{
+			Configuration: amplitude.NewConfig("test-development-api-key"),
+		},
+	})
+
+	// Amplitude.Client.Logger is an interface of which equality is determined based on memory address
+	// So here only check one of the default configurations, FlushQueueSize
+	assert.Equal(t, expectConfig.APIKey, instance.Client.GetConfig().APIKey)
+	assert.Equal(t, expectConfig.FlushQueueSize, instance.Client.GetConfig().FlushQueueSize)
 }
