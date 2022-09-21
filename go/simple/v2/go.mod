bab70c397c1f6741dbcb48041eaef0c6cccec23e
Andrey Sokolov <andrey.sokolov@amplitude.com>
2022-09-21 21:24:46 +0400

    AMP-58494 Add tests and GitHub actions

--- go/simple/v2/go.mod
+++ go/simple/v2/go.mod
@@ -12,5 +12,6 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- go/simple/v2/tests/ampli_test.go
+++ go/simple/v2/tests/ampli_test.go
@@ -6,39 +6,101 @@ import (
 	"github.com/amplitude/analytics-go/amplitude"
 	"github.com/amplitude/analytics-go/amplitude/constants"
 	"github.com/amplitude/analytics-go/amplitude/types"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/suite"
 
 	"ampli-example/ampli"
 )
 
-func TestIdentify(t *testing.T) {
-	identify := ampli.Identify.Builder().
-		RequiredNumber(6.4).
-		OptionalArray([]string{"a", "b"}).Build()
-
-	userProperties := map[amplitude.IdentityOp]map[string]interface{}{}
-	userProperties[types.IdentityOpSet] = map[string]interface{}{
-		"requiredNumber": 6.4,
-		"optionalArray":  []string{"a", "b"},
-	}
-	assert.Equal(t, userProperties, identify.ToAmplitudeEvent().UserProperties)
+func TestAmpliSuite(t *testing.T) {
+	suite.Run(t, new(AmpliSuite))
 }
 
-func TestGroup(t *testing.T) {
-	group := ampli.Group.Builder().RequiredBoolean(true).Build()
+type AmpliSuite struct {
+	suite.Suite
+}
 
-	expectEvent := amplitude.Event{
+func (t *AmpliSuite) TestIdentify() {
+	testAmpli, client := t.createAmpli()
+
+	expectedEvent := amplitude.Event{
+		EventType: amplitude.IdentifyEventType,
+		UserProperties: map[types.IdentityOp]map[string]interface{}{
+			types.IdentityOpSet: {"requiredNumber": 6.4, "optionalArray": []string{"a", "b"}},
+		},
+		EventOptions: amplitude.EventOptions{UserID: "user-1"},
+	}
+
+	client.On("Track", expectedEvent).Return().Once()
+
+	testAmpli.Identify("user-1", ampli.Identify.Builder().
+		RequiredNumber(6.4).
+		OptionalArray([]string{"a", "b"}).
+		Build(),
+	)
+
+	client.AssertExpectations(t.T())
+}
+
+func (t *AmpliSuite) TestGroupIdentify() {
+	testAmpli, client := t.createAmpli()
+
+	expectedEvent := amplitude.Event{
 		EventType: amplitude.GroupIdentifyEventType,
 		GroupProperties: map[amplitude.IdentityOp]map[string]interface{}{
 			types.IdentityOpSet: {"requiredBoolean": true},
 		},
+		Groups: map[string][]string{
+			"group-type": {"group-name"},
+		},
 	}
 
-	assert.Equal(t, expectEvent, group.ToAmplitudeEvent())
+	client.On("Track", expectedEvent).Return().Once()
+
+	testAmpli.GroupIdentify("group-type", "group-name", ampli.Group.Builder().
+		RequiredBoolean(true).
+		Build(),
+	)
+
+	client.AssertExpectations(t.T())
 }
 
-func TestEventWithAllProperties(t *testing.T) {
-	event := ampli.EventWithAllProperties.Builder().
+func (t *AmpliSuite) TestSetGroup() {
+	testAmpli, client := t.createAmpli()
+
+	client.On("SetGroup",
+		"group-type", []string{"group-name-1", "group-name-2"}, amplitude.EventOptions{UserID: "user-1"},
+	).Return().Once()
+
+	testAmpli.SetGroup("user-1", "group-type", []string{"group-name-1", "group-name-2"})
+
+	client.AssertExpectations(t.T())
+}
+
+func (t *AmpliSuite) TestEventWithAllProperties() {
+	testAmpli, client := t.createAmpli()
+
+	expectedEvent := amplitude.Event{
+		EventType: "Event With All Properties",
+		EventProperties: map[string]interface{}{
+			"requiredArray":   []string{"abc", "test"},
+			"requiredBoolean": true,
+			"requiredConst":   "some-const-value",
+			"requiredEnum":    ampli.EventWithAllPropertiesRequiredEnum("Enum1"),
+			"requiredInteger": 3,
+			"requiredNumber":  16.4,
+			"requiredString":  "str",
+			"optionalString":  "optional-string",
+		},
+		EventOptions: amplitude.EventOptions{
+			UserID:   "user-1",
+			DeviceID: "device-id",
+		},
+	}
+
+	client.On("Track", expectedEvent).Return().Once()
+
+	testAmpli.Track("user-1", ampli.EventWithAllProperties.Builder().
 		RequiredArray([]string{"abc", "test"}).
 		RequiredBoolean(true).
 		RequiredEnum(ampli.EventWithAllProperties.RequiredEnum.Enum1).
@@ -46,33 +108,45 @@ func TestEventWithAllProperties(t *testing.T) {
 		RequiredNumber(16.4).
 		RequiredString("str").
 		OptionalString("optional-string").
-		Build()
+		Build(),
+		ampli.EventOptions{DeviceID: "device-id"},
+	)
 
-	eventProperties := map[string]interface{}{
-		"requiredArray":   []string{"abc", "test"},
-		"requiredBoolean": true,
-		`requiredConst`:   `some-const-value`,
-		"requiredEnum":    ampli.EventWithAllProperties.RequiredEnum.Enum1,
-		"requiredInteger": 3,
-		"requiredNumber":  16.4,
-		"requiredString":  "str",
-		"optionalString":  "optional-string",
-	}
-	assert.Equal(t, eventProperties, event.ToAmplitudeEvent().EventProperties)
+	client.AssertExpectations(t.T())
 }
 
-func TestAmpli_LoadWithEnvironment(t *testing.T) {
+func (t *AmpliSuite) TestFlush() {
+	testAmpli, client := t.createAmpli()
+
+	client.On("Flush").Return().Once()
+
+	testAmpli.Flush()
+
+	client.AssertExpectations(t.T())
+}
+
+func (t *AmpliSuite) TestShutdown() {
+	testAmpli, client := t.createAmpli()
+
+	client.On("Shutdown").Return().Once()
+
+	testAmpli.Shutdown()
+
+	client.AssertExpectations(t.T())
+}
+
+func (t *AmpliSuite) TestLoadWithEnvironment() {
 	instance := ampli.Ampli{}
 	ampli.APIKey[ampli.EnvironmentDevelopment] = "test-development-api-key"
 	instance.Load(ampli.LoadOptions{
 		Environment: ampli.EnvironmentDevelopment,
 	})
 
-	assert.Equal(t, "test-development-api-key", instance.Client.Config().APIKey)
-	assert.Equal(t, constants.DefaultConfig.FlushQueueSize, instance.Client.Config().FlushQueueSize)
+	t.Assert().Equal("test-development-api-key", instance.Client.Config().APIKey)
+	t.Assert().Equal(constants.DefaultConfig.FlushQueueSize, instance.Client.Config().FlushQueueSize)
 }
 
-func TestAmpli_LoadWithClient(t *testing.T) {
+func (t *AmpliSuite) TestLoadWithClient() {
 	instance := ampli.Ampli{}
 	instance.Load(ampli.LoadOptions{
 		Client: ampli.LoadClientOptions{
@@ -80,11 +154,11 @@ func TestAmpli_LoadWithClient(t *testing.T) {
 		},
 	})
 
-	assert.Equal(t, "test-development-api-key", instance.Client.Config().APIKey)
-	assert.Equal(t, constants.DefaultConfig.FlushQueueSize, instance.Client.Config().FlushQueueSize)
+	t.Assert().Equal("test-development-api-key", instance.Client.Config().APIKey)
+	t.Assert().Equal(constants.DefaultConfig.FlushQueueSize, instance.Client.Config().FlushQueueSize)
 }
 
-func TestAmpli_LoadWithInstance(t *testing.T) {
+func (t *AmpliSuite) TestLoadWithInstance() {
 	instance := ampli.Ampli{}
 	instance.Load(ampli.LoadOptions{
 		Client: ampli.LoadClientOptions{
@@ -92,11 +166,11 @@ func TestAmpli_LoadWithInstance(t *testing.T) {
 		},
 	})
 
-	assert.Equal(t, "test-development-api-key", instance.Client.Config().APIKey)
-	assert.Equal(t, constants.DefaultConfig.FlushQueueSize, instance.Client.Config().FlushQueueSize)
+	t.Assert().Equal("test-development-api-key", instance.Client.Config().APIKey)
+	t.Assert().Equal(constants.DefaultConfig.FlushQueueSize, instance.Client.Config().FlushQueueSize)
 }
 
-func TestAmpli_LoadWithConfig(t *testing.T) {
+func (t *AmpliSuite) TestLoadWithConfig() {
 	instance := ampli.Ampli{}
 	instance.Load(ampli.LoadOptions{
 		Client: ampli.LoadClientOptions{
@@ -104,6 +178,60 @@ func TestAmpli_LoadWithConfig(t *testing.T) {
 		},
 	})
 
-	assert.Equal(t, "test-development-api-key", instance.Client.Config().APIKey)
-	assert.Equal(t, constants.DefaultConfig.FlushQueueSize, instance.Client.Config().FlushQueueSize)
+	t.Assert().Equal("test-development-api-key", instance.Client.Config().APIKey)
+	t.Assert().Equal(constants.DefaultConfig.FlushQueueSize, instance.Client.Config().FlushQueueSize)
+}
+
+func (t *AmpliSuite) createAmpli() (*ampli.Ampli, *mockClient) {
+	instance := new(ampli.Ampli)
+	client := &mockClient{}
+	instance.Load(ampli.LoadOptions{
+		Client: ampli.LoadClientOptions{Instance: client},
+	})
+	return instance, client
+}
+
+type mockClient struct {
+	mock.Mock
+}
+
+func (m *mockClient) Track(event amplitude.Event) {
+	m.Called(event)
+}
+
+func (m *mockClient) Identify(identify amplitude.Identify, eventOptions amplitude.EventOptions) {
+	m.Called(identify, eventOptions)
+}
+
+func (m *mockClient) GroupIdentify(groupType string, groupName string, identify amplitude.Identify, eventOptions amplitude.EventOptions) {
+	m.Called(groupType, groupName, identify, eventOptions)
+}
+
+func (m *mockClient) SetGroup(groupType string, groupName []string, eventOptions amplitude.EventOptions) {
+	m.Called(groupType, groupName, eventOptions)
+}
+
+func (m *mockClient) Revenue(revenue amplitude.Revenue, eventOptions amplitude.EventOptions) {
+	m.Called(revenue, eventOptions)
+}
+
+func (m *mockClient) Flush() {
+	m.Called()
+}
+
+func (m *mockClient) Shutdown() {
+	m.Called()
+}
+
+func (m *mockClient) Add(plugin amplitude.Plugin) {
+	m.Called(plugin)
+}
+
+func (m *mockClient) Remove(pluginName string) {
+	m.Called(pluginName)
+}
+
+func (m *mockClient) Config() amplitude.Config {
+	args := m.Called()
+	return args[0].(amplitude.Config)
 }
