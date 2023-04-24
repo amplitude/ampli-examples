f059c95214f3e346b20b3113f70331824d43b8b0
Andrey Sokolov <andrey.sokolov@amplitude.com>
2023-04-24 20:43:21 +0400

    AMP-73622 Update Segment plugin example (Node) to support flush()

--- node/typescript/v2/AmpliApp/src/app.ts
+++ node/typescript/v2/AmpliApp/src/app.ts
@@ -194,13 +194,6 @@ async function sendEvents() {
    * Wait for returned promise to complete
    */
   await ampli.flush().promise;
-
-  /**
-   * Flush all pending Segment events
-   *
-   * Wait for returned promise to complete
-   */
-  // await segmentPlugin.segment.flush();
 }
 
 initAmpli().then(
